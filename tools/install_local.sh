8c46560d142079145788673ac05297f99fada6ab
ziqiangpei <ziqiangpei@foxmail.com>
2023-03-17 22:45:05 +0800

    fix ci
    Signed-off-by: ziqiangpei <ziqiangpei@foxmail.com>

--- tools/install_local.sh
+++ tools/install_local.sh
@@ -161,7 +161,8 @@ function _omb_install_user_bashrc {
 export OSH='${OSH//\'/\'\\\'\'}'
   " "$OSH"/templates/bashrc.osh-template >| ~/.bashrc.omb-temp &&
     _omb_install_run mv -f ~/.bashrc.omb-temp ~/.bashrc &&
-    _omb_install_run cp -a ${OSH}/templates/setup_env.sh.osh-template ~/setup_env.sh
+    "$OSH"/templates/setup_env.sh.osh-template >| ~/setup_env.sh.omb-temp &&
+    _omb_install_run mv -f ~/setup_env.sh.omb-temp ~/setup_env.sh
 
   # If "source ~/.bashrc" is not found in ~/.bash_profile or ~/.profile, we try
   # to create a new ~/.bash_profile with the default content or show messages
